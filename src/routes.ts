5bbed4a0bbea701dbd3854a22bcbfd3f07d4947c
anderson <andersonszdc@gmail.com>
2021-12-22 12:13:09 -0300

    finished CRUD

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
+import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
+import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
 
 const router = Router();
 
 router.post("/categories", new CreateCategoryController().handle);
+router.get("/categories", new GetAllCategoriesController().handle);
+router.delete("/categories/:id", new DeleteCategoryController().handle);
+router.put("/categories/:id", new UpdateCategoryController().handle);
 
 export { router };
